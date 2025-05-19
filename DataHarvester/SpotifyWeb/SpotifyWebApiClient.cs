8c0358daf39271c20a84c43202ddab5becd8e531
Brecht <brecht.spincemaill@vdabcampus.be>
2025-05-20 01:09:11 +0200

    added retry strategy to spotify apicalls

--- DataHarvester/SpotifyWeb/SpotifyWebApiClient.cs
+++ DataHarvester/SpotifyWeb/SpotifyWebApiClient.cs
@@ -1,6 +1,9 @@
 ﻿using DataHarvester.Models;
 using MassTransit;
+using System.Threading.Tasks;
 using MassTransit.JobService.Messages;
+using Polly;
+using Polly.Retry;
 using System;
 using System.Net;
 using System.Net.Http.Headers;
@@ -9,15 +12,39 @@ using System.Threading.RateLimiting;
 
 namespace DataHarvester.SpotifyWeb;
 
-public class SpotifyWebApiClient(
-    HttpClient httpclient,
-    SpotifyRateLimiter spotifyRateLimiter,
-    SpotifyWebApiClientTokenClient spotifyWebApiClientTokenClient) : ISpotifyWebApiClient
+public class SpotifyWebApiClient : ISpotifyWebApiClient
 {
-    private readonly HttpClient _httpClient = httpclient;
-    private readonly SlidingWindowRateLimiter _rateLimiter = spotifyRateLimiter.GetSlidingWindowRateLimiter();
+    private static readonly Random Jitterer = new();
+    private readonly HttpClient _httpClient;
+    private readonly SlidingWindowRateLimiter _rateLimiter;
+    private readonly ResiliencePipeline<HttpResponseMessage> _pipeline;
     private const string ApiUri = "https://api.spotify.com/v1";
-    private readonly SpotifyWebApiClientTokenClient _tokenClient = spotifyWebApiClientTokenClient;
+    private readonly SpotifyWebApiClientTokenClient _tokenClient;
+
+    public SpotifyWebApiClient(
+        HttpClient httpclient,
+        SpotifyRateLimiter spotifyRateLimiter,
+        SpotifyWebApiClientTokenClient spotifyWebApiClientTokenClient)
+    {
+        _httpClient = httpclient;
+        _rateLimiter = spotifyRateLimiter.GetSlidingWindowRateLimiter();
+        _tokenClient = spotifyWebApiClientTokenClient;
+        _pipeline = new ResiliencePipelineBuilder<HttpResponseMessage>()
+            .AddRetry(new()
+            {
+                ShouldHandle = args => ValueTask.FromResult(
+                    args.Outcome.Exception is not null ||
+                    args.Outcome.Result?.StatusCode == HttpStatusCode.TooManyRequests),
+                MaxRetryAttempts = 5,
+                DelayGenerator = args =>
+                {
+                    var jitter = Jitterer.NextDouble() + 1; //1 to 2
+                    return new ValueTask<TimeSpan?>(
+                        TimeSpan.FromSeconds(10 * jitter * Math.Pow(2, args.AttemptNumber)));
+                }
+            })
+            .Build();
+    }
 
     private async Task<string> GetSpotifyRequestAsync(string requestUri)
     {
@@ -25,10 +52,12 @@ public class SpotifyWebApiClient(
         using var lease = await _rateLimiter.AcquireAsync(1);
         var token = await _tokenClient.GetTokenAsync();
 
-        var request = new HttpRequestMessage(HttpMethod.Get,
-            requestUri);
-        request.Headers.Add("Authorization", $"{token.Token_type} {token.Access_token}");
-        var response = await _httpClient.SendAsync(request);
+        var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
+        request.Headers.Authorization =
+            new AuthenticationHeaderValue(token.Token_type, token.Access_token);
+
+        var response = await _pipeline.ExecuteAsync<HttpResponseMessage>(async ct =>
+            await _httpClient.SendAsync(request, ct));
 
         switch (response.StatusCode)
         {
@@ -39,7 +68,7 @@ public class SpotifyWebApiClient(
                 }
             case HttpStatusCode.BadRequest:
                 {
-                    throw new ArgumentException($"invalid Uri {requestUri}",nameof(requestUri));
+                    throw new ArgumentException($"invalid Uri {requestUri}", nameof(requestUri));
                 }
             default:
                 throw new NotImplementedException(response.StatusCode.ToString());
