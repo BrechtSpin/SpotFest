5831c4e5d84a597233beb9d3bcc79152fa583d20
Brecht <brecht.spincemaill@vdabcampus.be>
2025-05-21 04:07:39 +0200

    more typos

--- Frontend/src/app/services/happeningHub.service.ts
+++ Frontend/src/app/services/happeningHub.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from '@angular/core';
-import { HubConnection, HubConnectionBuilder, HubConnectionState, LogLevel } from '@microsoft/signalR';
+import { HubConnection, HubConnectionBuilder, HubConnectionState, LogLevel } from '@microsoft/signalr';
 
 import { environment } from '@env/environment';
 
