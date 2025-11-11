6fc8c7735c204ec06260b5bc87a30153e3669bf1
Brecht <brecht.spincemaill@vdabcampus.be>
2025-11-11 21:02:01 +0100

    Added endpoint + service /me to retrieve user of logged in user.

--- UserAuthService/Services/IAuthService.cs
+++ UserAuthService/Services/IAuthService.cs
@@ -6,4 +6,5 @@ public interface IAuthService
 {
     Task<(bool, string)> Register(RegisterDTO registerDTO);
     Task<(AuthResponseDto authResponseDto, string? token)> LoginAttempt(LoginDTO loginDTO);
+    Task<AuthResponseDto> GetCurrentUser(string? userId);
 }
