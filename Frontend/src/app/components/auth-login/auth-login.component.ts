af4e4e2c9fe2cc5f4590feee4657728a3f8d98d9
Brecht <brecht.spincemaill@vdabcampus.be>
2025-11-17 06:37:15 +0100

    added message when redirected to loginpage

--- Frontend/src/app/components/auth-login/auth-login.component.ts
+++ Frontend/src/app/components/auth-login/auth-login.component.ts
@@ -1,8 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '@services/auth.service';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'auth-login',
@@ -10,10 +10,11 @@ import { Router, RouterLink } from '@angular/router';
   imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './auth-login.component.html'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   private fb = inject(FormBuilder);
   private authService = inject(AuthService);
   private router = inject(Router)
+  private route = inject(ActivatedRoute);
 
   loginForm = this.fb.nonNullable.group({
     email: ['', [Validators.required, Validators.email]],
@@ -21,6 +22,20 @@ export class LoginComponent {
   });
   isLoading = false;
   errorMessage = '';
+
+
+  alertMessage: string | null = null;
+  alertType: string = 'warning'; // Bootstrap alert type
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      const msg = params['message'];
+
+      if (msg === 'login-required') {
+        this.alertType = 'warning';
+        this.alertMessage = 'You must be logged in to access that page.';
+      }
+    });
+  }
 
   isInvalid(controlName: string) {
     const ctrl = this.loginForm.get(controlName)!;
